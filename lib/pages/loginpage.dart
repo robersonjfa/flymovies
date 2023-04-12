9b3ea81cceff020c0078ed26c834f1c0c1e93db3
Roberson Alves <robersonjfa@gmail.com>
2023-04-12 11:35:33 -0300

    Adicionado suporte a localização/internacionalização.

--- lib/pages/loginpage.dart
+++ lib/pages/loginpage.dart
@@ -84,10 +84,12 @@ class _LoginPageState extends State<LoginPage> {
       userValidator: (value) {
         // TODO: precisa ser um e-mail válido/
         if (value!.isEmpty) return 'E-mail inválido';
+        return null;
       },
       passwordValidator: (value) {
         // TODO: Validar a senha, com no mínimo 6 caracteres(letras e números)
         if (value!.isEmpty) return 'Senha inválida';
+        return null;
       },
       loginProviders: <LoginProvider>[
         LoginProvider(
