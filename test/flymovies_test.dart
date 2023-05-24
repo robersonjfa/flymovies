9405e0acb76be4448a469b3867f5cc55faab2dbc
Roberson Alves <robersonjfa@gmail.com>
2023-05-24 18:52:10 -0300

    Adicionados testes

--- test/flymovies_test.dart
+++ test/flymovies_test.dart
@@ -1,10 +1,16 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flymovies/models/movie.dart';
-import 'package:flymovies/models/user.dart';
-import 'package:flymovies/pages/main_page.dart';
+import 'package:flymovies/pages/settings_page.dart';
+import 'package:flymovies/pages/splash_page.dart';
 import 'package:flymovies/utils/utils.dart';
 
 void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
   group('FlyMovies Test', () {
     test('Test utils genres', () {
       Movie movie = Movie(
@@ -18,20 +24,16 @@ void main() {
           genreIds: [28, 12]);
 
       var genres = Utils.getGenres(movie);
-      expect(genres, 'Action, Adventure');
+      expect(genres, 'Action, Adventure ');
     });
 
-    testWidgets('MyWidget has a title', (tester) async {
-      final user = User(
-          name: 'teste',
-          email: 'teste',
-          password:
-              'teste'); // Create the widget by telling the tester to build it.
-      await tester.pumpWidget(MainPage(title: 'Tela principal', user: user));
+    testWidgets('MyWidget has a widget', (tester) async {
+      await tester
+          .pumpWidget(const MaterialApp(home: Scaffold(body: SettingsPage())));
+      //await tester.pumpWidget(const SplashPage());
+      final hasIndicator = find.byType(SingleChildScrollView);
 
-      final titleFinder = find.text('Tela principal');
-
-      expect(titleFinder, findsOneWidget);
+      expect(hasIndicator, findsOneWidget);
     });
   });
 }
